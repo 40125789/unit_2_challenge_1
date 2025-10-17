84d3a8e8430287a8ae9c3297a3bcef9db47669ba
Ben Stephenson <benstephensonwork@gmail.com>
2025-10-17 15:26:50 +0100

    Add tests for HighValue

--- tests/test_high_value.py
+++ tests/test_high_value.py
@@ -6,23 +6,23 @@ def test_initial_values_are_set():
     assert hv.value_first == 10
     assert hv.value_second == 20
 
-#Test for highest value
+#Test for highest value for first value
 def test_first_highest_value():
     hv = HighValue(8, 5)
     assert hv.get_highest() == "First value is higher"
     
     
-#Test for highest value
+#Test for highest value for second value
 def test_second_highest_value():
     hv = HighValue(5, 8)
     assert hv.get_highest() == "Second value is higher"
     
-#Test for highest value
+#Test for highest value for equal values
 def test_equal_values():
     hv = HighValue(8, 8)
     assert hv.get_highest() == "Values are equal"
     
-#Test add function
+#Test add function for first value
 def test_add_increases_first_value():
     hv = HighValue(3, 10)
     hv.add(3, "first")
@@ -36,3 +36,24 @@ def test_add_increases_second_value():
     hv.add(3, "second")
     assert hv.value_first == 3
     assert hv.value_second == 13
+
+#Test add function where adding changes highest value
+def test_add_changes_highest_value_to_first():
+    hv = HighValue(5, 8)
+    assert hv.get_highest() == "Second value is higher"
+    hv.add(5, "first")
+    assert hv.get_highest() == "First value is higher"
+
+#Test add function where adding changes highest value
+def test_add_changes_highest_value_to_second():
+    hv = HighValue(8, 5)
+    assert hv.get_highest() == "First value is higher"
+    hv.add(5, "second")
+    assert hv.get_highest() == "Second value is higher"
+
+#Test add function where adding changes highest value to become equal
+def test_add_changes_highest_value_to_equals():
+    hv = HighValue(8, 5)
+    assert hv.get_highest() == "First value is higher"
+    hv.add(3, "second")
+    assert hv.get_highest() == "Values are equal"
